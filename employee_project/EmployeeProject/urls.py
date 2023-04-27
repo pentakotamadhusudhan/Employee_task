f8e09b957eaf7b9fc6a8c4859e9b58bac927b903
Madhu <pentakotamadhu74@gmail.com>
2023-04-27 23:46:27 +0530

    madhu

--- employee_project/EmployeeProject/urls.py
+++ employee_project/EmployeeProject/urls.py
@@ -37,9 +37,9 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('employee/',include("EmployeeCrud.urls")),
-    # path('goal/',include("routesApp.urls")),
-    path('routesApp/',include("routesApp.urls")),
+    path('employee/',include("EmployeeCrud.urls")),
+    # # path('goal/',include("routesApp.urls")),
+    # path('routesApp/',include("routesApp.urls")),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
